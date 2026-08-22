0118a24feaa5580c66177abbf773d7b1609b8a0f
deepaksugandhi-web <deepak.sugandhi@gmail.com>
2026-08-22 23:20:45 +0530

    chore: scaffold eframe project

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Speed FileMan",
+        options,
+        Box::new(|_cc| Ok(Box::new(FileManApp::default()))),
+    )
+}
+
+#[derive(Default)]
+struct FileManApp {}
+
+impl eframe::App for FileManApp {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ui, |ui| {
+            ui.label("Speed FileMan");
+        });
+    }
+}
